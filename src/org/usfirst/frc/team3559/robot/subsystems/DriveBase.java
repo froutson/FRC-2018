bcf5fb2afa80828c1f63c68e5fe042dd054be481
Fred Routson <froutson@gmail.com>
2018-01-16 11:59:19 -0500

    1st iteration of DriveBase.java

--- src/org/usfirst/frc/team3559/robot/subsystems/DriveBase.java
+++ src/org/usfirst/frc/team3559/robot/subsystems/DriveBase.java
@@ -2,58 +2,73 @@
  * DriveBase.class for 2018 Robot
  * Created by Fred Routson
  * Creation Date: 14 January 2018
+ * Modified: 16 January 2018, Fred Routson
+ * Note(s): 1st iteration just sets up basic movement w/o feedback
  */
 
 package org.usfirst.frc.team3559.robot.subsystems;
 
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+//import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-
-
 
 /**
  *
  */
+
 public class DriveBase extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-	
-	private TalonSRX m_leftMaster, m_rightMaster;
-	private TalonSRX m_leftSlave, m_rightSlave;
-	//private RobotDrive drive;
+	private WPI_TalonSRX m_leftMaster, m_rightMaster;
+	private WPI_TalonSRX m_leftSlave, m_rightSlave;
+	private DifferentialDrive drive;
 	//private boolean brake = true;						//May not be needed??
 	private double speedModifier = 0.8;					//Adjust the max input for speed limiting.
 	
 	public DriveBase(){
 		super();
-		m_leftMaster = new TalonSRX(1);
+		m_leftMaster = new WPI_TalonSRX(0);
 		m_leftMaster.setNeutralMode(NeutralMode.Brake);
-		m_leftSlave = new TalonSRX(2);
+		m_leftSlave = new WPI_TalonSRX(1);
 		m_leftSlave.setNeutralMode(NeutralMode.Brake);
 		m_leftSlave.set(ControlMode.Follower, m_leftMaster.getDeviceID());
 		// TODO: Add code for sensor input/feedback to close the loop
 		
-		m_rightMaster = new TalonSRX(3);
+		m_rightMaster = new WPI_TalonSRX(2);
 		m_rightMaster.setNeutralMode(NeutralMode.Brake);
-		m_rightSlave = new TalonSRX(4);
+		m_rightSlave = new WPI_TalonSRX(3);
 		m_rightSlave.setNeutralMode(NeutralMode.Brake);
 		m_rightSlave.set(ControlMode.Follower, m_rightMaster.getDeviceID());
 		// TODO: Add code for sensor input/feedback. Close the loop!
 		
-		
-		
-		
-		
+		drive = new DifferentialDrive(m_leftMaster, m_rightMaster);
+		drive.setSafetyEnabled(true);
+			
 	}
 		
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        // setDefaultCommand(new TankDriveWithGamepad());
     }
+    
+    public void drive(double left, double right) {
+    	drive.tankDrive(-left, -right);
+    }
+    
+    public void stop() {
+    	this.drive(0.0, 0.0);
+    }
+    
+    /**
+     * Code to drive with gamepad
+     */
+    public void drive(Joystick gamepad) {
+    	drive(speedModifier*gamepad.getY(), speedModifier*gamepad.getRawAxis(3));
+    }
+   
 }
 
